37ecd418288ef9de69333a901491377b60f39a4d
Mattias Hemmingsson <matte@elino.se>
2015-10-17 21:29:01 +0200

    working

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,15 +1,25 @@
-rom fabric.api import run, env, local
+from fabric.api import run, env, local
 from fabric.context_managers import cd
+from fabric.operations import put, sudo
 
 env.user='ubuntu'
 
 
 def deploy():
     with cd('/asylguiden'):
+
+        #Copy new files
+        sudo('mkdir /asylguiden_d')
+        sudo('chown ubuntu:ubuntu /asylguiden_d')
+        put('../*', '/asylguiden_d/')
+        #run('cp /asylguiden_prod/nginx/tls/asylguiden.crt /asylguiden/nginx/ssl/asylguiden/server.crt')
+        #run('cp /asylguiden_prod/nginx/tls/asylguiden.key /asylguiden/nginx/ssl/asylguiden/server.key')
+        #run('cp /asylguiden_prod/nginx/tls/bundle.crt /asylguiden/nginx/ssl/asylguiden/bundle.crt')
+        #Stoping and buildning new images
         run('docker-compose stop')
-        local('rsync -avz * {0}@{1}:/asylguiden/'.format(env.user,env.hosts))
-        run('cp /asylguiden_prod/nginx/tls/asylguiden.crt /asylguiden/nginx/ssl/asylguiden/server.crt')
-        run('cp /asylguiden_prod/nginx/tls/asylguiden.key /asylguiden/nginx/ssl/asylguiden/server.key')
-        run('cp /asylguiden_prod/nginx/tls/bundle.crt /asylguiden/nginx/ssl/asylguiden/bundle.crt')
+        sudo('cp -rf /asylguiden_d /asylguiden')
         run('docker-compose build')
         run('docker-compose start')
+
+        #clan out
+        sudo('rm -rf /asylguiden_d')
